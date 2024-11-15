b611b3a11e931bf08e3322cbae2d41ddbad6a79f
AghnatHs <aghnathasya@gmail.com>
2024-11-15 11:29:04 +0700

    FIX: add pathresolve at database path to avoid file not found

--- src/database/db.js
+++ src/database/db.js
@@ -1,5 +1,6 @@
 const Database = require("better-sqlite3");
-const db = new Database(process.env.DB_URL, Database.OPEN_READWRITE, {
+const path = require("path");
+const db = new Database(path.resolve(process.env.DB_URL), Database.OPEN_READWRITE, {
   verbose: (sql) => console.log(`Query: ${sql} ${Date.now()}`),
 });
 
